df026b3851e7c0004426f350d90324078fef9597
Marcela <mirandamarcela578@gmail.com>
2025-10-22 17:28:57 -0400

    fix/ ajustes de las entidades

--- backend_tienda_textiles/src/productos/entities/producto.entity.ts
+++ backend_tienda_textiles/src/productos/entities/producto.entity.ts
@@ -1,5 +1,6 @@
 import { CarritoProductos } from 'src/carrito-productos/entities/carrito-producto.entity';
 import { Categoria } from 'src/categorias/entities/categoria.entity';
+import { PedidoProducto } from 'src/pedido_productos/entities/pedido_producto.entity';
 import {
   Column,
   CreateDateColumn,
@@ -50,4 +51,7 @@ export class Producto {
 
   @OneToMany(() => CarritoProductos, carritoProductos => carritoProductos.producto)
   carritoProductos: CarritoProductos[];
+
+  @OneToMany(() => PedidoProducto, pedidoProducto => pedidoProducto.producto)
+  pedidosProductos: PedidoProducto[];
 }
