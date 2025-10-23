6a369e3aa7d65074e17d07574f02c702ea34f7a1
RamiroDCuizaM <ramirodavidcuizamurana@gmail.com>
2025-10-23 12:28:44 -0400

    feat implementacion cruds categorias y productos

--- frontend_tienda_textiles/src/main.ts
+++ frontend_tienda_textiles/src/main.ts
@@ -15,14 +15,14 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
-    theme: {
-        preset: Aura,
-        options: {
-            prefix: 'p',
-            darkModeSelector: 'system',
-            cssLayer: false
-        }
-    }
- });
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false,
+    },
+  },
+})
 
 app.mount('#app')
